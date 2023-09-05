ddb16f82822c3943e8cab277f478dcbbf6a4c659
Matias Reynolds <matiasreynolds1995@gmail.com>
2023-09-05 12:26:55 -0300

    Segundo commit

---
+++ app.js
@@ -0,0 +1,44 @@
+require('colors');
+const Tareas = require('./models/tareas');
+const { inquirerMenu,
+    pausa,
+    leerInput
+} = require('./helpers/inquirer');
+const {guardarDB, leerDB} = require('./db/guardarArchivo');
+
+
+const main = async() => {
+    let opt = '';
+
+    const tareas = new Tareas;
+
+    const tareasDB = leerDB();
+
+    if(tareasDB) {
+        //Establecer tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    do {
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea( desc );
+            break;
+            case '2':
+                tareas.listadoCompleto();
+            break;
+
+        };
+        
+        guardarDB( tareas.listadoArr );
+
+        await pausa();
+        
+       
+    
+    } while ( opt !== '0');
+}
+
+main();
